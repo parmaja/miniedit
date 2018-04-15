e5fe250930e68168a0965e42b4d0bd96185ea5a8
Zaher Dirkey <zaherdirkey@yahoo.com>
2018-04-16 02:51:29 +0300

    Fix: running file of Sard

--- source/extends/sard/mneSARDClasses.pas
+++ source/extends/sard/mneSARDClasses.pas
@@ -11,8 +11,8 @@ interface
 
 uses
   Messages, Forms, SysUtils, StrUtils, Variants, Classes, Controls, Graphics, Contnrs, Dialogs,
-  LCLintf, LCLType,
-  EditorOptions, EditorDebugger,
+  LCLintf, LCLType, FileUtil,
+  EditorOptions, EditorDebugger, EditorRun, EditorClasses, mneRunFrames,
   SynEditHighlighter, SynEditSearch, SynEdit, Registry, EditorEngine, mnXMLRttiProfile, mnXMLUtils,
   SynEditTypes, SynCompletion, SynHighlighterHashEntries, EditorProfiles,
   mnSynHighlighterSARD, SynHighlighterLFM;
@@ -48,10 +48,14 @@ type
 
   { TSARDPerspective }
 
+  { TSARDTendency }
+
   TSARDTendency = class(TEditorTendency)
   protected
     procedure Init; override;
     function CreateDebugger: TEditorDebugger; override;
+    procedure CreateOptionsFrame(AOwner: TComponent; ATendency: TEditorTendency; AddFrame: TAddFrameCallBack); override;
+    procedure DoRun(Info: TmneRunInfo); override;
   public
   end;
 
@@ -75,6 +79,75 @@ function TSARDTendency.CreateDebugger: TEditorDebugger;
 begin
   //Result := TSardEditorDebugger.Create;
   Result := inherited CreateDebugger;
+end;
+
+procedure TSARDTendency.CreateOptionsFrame(AOwner: TComponent; ATendency: TEditorTendency; AddFrame: TAddFrameCallBack);
+var
+  aFrame: TRunFrameOptions;
+begin
+  aFrame := TRunFrameOptions.Create(AOwner);
+  aFrame.Options := ATendency.RunOptions;
+  EnumRunCommands(aFrame.CommandEdit.Items);
+  aFrame.Caption := 'Options';
+  AddFrame(aFrame);
+end;
+
+procedure TSARDTendency.DoRun(Info: TmneRunInfo);
+var
+  aRunItem: TmneRunItem;
+begin
+  Engine.Session.Run.Clear;
+
+  if rnaExecute in Info.Actions then
+  begin
+    Engine.SendAction(eaClearOutput);
+
+    aRunItem := Engine.Session.Run.Add;
+    aRunItem.Info.Run.Pause := Info.Pause;
+    aRunItem.Info.Run.Console := Info.Console;
+    aRunItem.Info.Title := ExtractFileNameWithoutExt(Info.MainFile);
+    aRunItem.Info.CurrentDirectory := Info.Root;
+    aRunItem.Info.StatusMessage := 'Runing ' + Info.MainFile;
+    {
+    if RunOptions.Require <> '' then
+      RunItem.Info.Run.AddParam('-l '+ RunOptions.Require);
+    if rnaDebug in Info.Actions then
+      aRunItem.Info.Run.AddParam('-e '+ '"require(''mobdebug'').start()"'); //using mobdebug
+    }
+    aRunItem.Info.Run.Command := Info.Command;
+    if Info.Command = '' then
+    begin
+      {$ifdef windows}
+        aRunItem.Info.Run.Command := 'sard.exe';
+      {$else}
+        aRunItem.Info.Run.Command := 'sard';
+      {$endif}
+      aRunItem.Info.Run.AddParam(' "' + Info.MainFile + '"');
+    end
+  end
+  else if (rnaLint in Info.Actions) then
+  begin
+    Engine.SendAction(eaClearOutput);
+
+    aRunItem := Engine.Session.Run.Add;
+    aRunItem.Info.Title := ExtractFileNameWithoutExt(Info.MainFile);
+    aRunItem.Info.CurrentDirectory := Info.Root;
+    aRunItem.Info.Run.Silent := True;
+    aRunItem.Info.Run.Console := False;
+
+    aRunItem.Info.StatusMessage := 'Linting ' + Info.MainFile;
+    aRunItem.Info.Run.AddParam('-l ');
+
+    {$ifdef windows}
+      aRunItem.Info.Run.Command := 'sard.exe';
+    {$else}
+      aRunItem.Info.Run.Command := 'sard';
+    {$endif}
+    aRunItem.Info.Run.AddParam(' "' + Info.MainFile + '"');
+  end;
+
+  if Engine.Session.Run.Active then //if there is items ready to run
+    Engine.Session.Run.Start(Self);
 end;
 
 { TSARDFileCategory }
