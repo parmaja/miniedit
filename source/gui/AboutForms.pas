d54e2ca7a85a881522de37031ef1fb4eb32527a1
Zaher Dirkey <zaherdirkey@yahoo.com>
2020-01-12 21:04:02 +0200

    Fix Version info in About

--- source/gui/AboutForms.pas
+++ source/gui/AboutForms.pas
@@ -10,7 +10,7 @@ interface
 
 uses
   Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  lclproc, lclintf, lclversion,
+  lclproc, lclintf, lclversion, fileinfo,
   Dialogs, StdCtrls, ExtCtrls, ComCtrls;
 
 type
@@ -20,22 +20,19 @@ type
   TAboutForm = class(TForm)
     Button1: TButton;
     Image2: TImage;
-    Label1: TLabel;
+    NameLbl: TLabel;
     Image1: TImage;
-    Memo1: TMemo;
     SiteLbl: TLabel;
-    VersionLbl: TLabel;
-    Label6: TLabel;
+    EnvVersionLbl: TLabel;
     Label8: TLabel;
-    Label2: TLabel;
+    PortionsLbl: TLabel;
     Label3: TLabel;
     Label4: TLabel;
-    Label5: TLabel;
-    Label7: TLabel;
     Label9: TLabel;
     Label10: TLabel;
     Label11: TLabel;
     Label12: TLabel;
+    VersionLbl: TLabel;
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure SiteLblClick(Sender: TObject);
@@ -53,9 +50,25 @@ begin
 end;
 
 procedure TAboutForm.FormCreate(Sender: TObject);
+var
+  s: string;
+  fv: TFileVersionInfo;
 begin
-  VersionLbl.Caption := ' Lazarus: '  + IntToStr(lcl_fullversion);// + ' FPC: ' + IntToStr(%FPC_FULLVERSION%);
-  //VersionInfo.GetFileVersion
+  s := ' Lazarus: '  + IntToStr(lcl_fullversion) + ' FPC: ' + IntToStr(FPC_FULLVERSION);
+
+  fv:=TFileVersionInfo.Create(nil);
+  try
+    fv.ReadFileInfo;
+    VersionLbl.Caption := fv.VersionStrings.Values['FileVersion'];
+    //writeln('Product version: ',fv.VersionStrings.Values['ProductVersion']);
+  finally
+    fv.Free;
+  end;
+
+  EnvVersionLbl.Caption := s;
+  Namelbl.Font.Size := 12;
+  VersionLbl.Font.Size := 12;
+  PortionsLbl.Font.Style := [fsBold];
 end;
 
 procedure TAboutForm.SiteLblClick(Sender: TObject);
